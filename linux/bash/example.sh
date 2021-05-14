c3ca073718cd6204b86a61b5a06e92b9afc70cc1
Travis <lxy_montage@163.com>
2021-05-14 14:04:35 +0800

    完成bash脚本作业

--- linux/bash/example.sh
+++ linux/bash/example.sh
@@ -1,12 +1,10 @@
-if [ $# = 0 ]
-then
-echo "Usage: $0 : <number>"
+if [ $# = 0 ]; then
+    echo "Usage: $0 : <number>"
 else
-count=$1
-while [ $count -gt 0 ]
-do
-count=`expr $count - 1`
-echo -e "\015 $count \c"
-sleep 1
-done
+    count=$1
+    while [ $count -gt 0 ]; do
+        count=$(expr $count - 1)
+        echo -e "\015 $count \c"
+        sleep 1
+    done
 fi
--- linux/bash/tcp_statistics.sh
+++ linux/bash/tcp_statistics.sh
@@ -1,28 +1,46 @@
 #!/usr/bin/bash
 clear
-interval=1
+interval=60
 str=`netstat --statistics | grep "[0-9][0-9]* segments received"`
 recv=$(expr "$str" : '^ *\([0-9][0-9]*\).*$')
-str=`netstat --statistics | grep "[0-9][0-9]* segments sent out"`
 out=$(expr "$str" : '^ *\([0-9][0-9]*\).*$')
 
-#Usage: output tcp_recv tcp_out recv out RECV OUT flag
+# comp pre_sum sum return
+comp()
+{
+	if [ $2 -gt $(expr $1 + 10) ]
+	then
+		eval "$3=\"+\""	
+	elif [ $2 -gt $(expr $1 - 10) ]
+	then
+		eval "$3=\" \""
+	else
+		eval "$3=\"-\""
+	fi
+}
+#Usage: output tcp_recv tcp_out recv out RECV OUT
 output()
 {
 	outnum=`expr $2 - $4`
 	recvnum=`expr $1 - $3`
 	sum=`expr $outnum + $recvnum`
+	if [ $7 -gt 0 ]
+	then
+		comp $pre_sum $sum "flag"
+    fi
+	pre_sum=$sum
 	eval "$5=$1";eval "$6=$2"
-	[ $7 -eq 0 ] && echo `date "+%Y-%m-%d %H:%M"` $outnum $recvnum $sum  
+	echo -e "\r" `date "+%Y-%m-%d %H:%M"` "\c"
+	printf "%4d %4d %4d %4c\n" $outnum $recvnum $sum $flag
 }
 
-
+index=0
 while (true)
-	do 
+	do
+	sleep $interval
 	str=`netstat --statistics | grep "[0-9][0-9]* segments received"`
 	tcp_recv=$(expr "$str" : '^ *\([0-9][0-9]*\).*$')
-	str=`netstat --statistics | grep "[0-9][0-9]* segments sent out"`
 	tcp_out=$(expr "$str" : '^ *\([0-9][0-9]*\).*$')
-	output $tcp_recv $tcp_out $recv $out "recv" "out" 0 0 
-	sleep $interval
+	output $tcp_recv $tcp_out $recv $out "recv" "out" $index
+	index=$(expr $index + 1)
 done
